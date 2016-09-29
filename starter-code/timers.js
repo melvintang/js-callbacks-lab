2e7b7c72d33e7035a8a1d8d365fcadee7677be02
Tang Peng <tchatpeng@gmail.com>
2016-09-29 11:13:26 +0800

    first trial

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,47 @@
-// your code here:
+
+
+
+document.addEventListener('DOMContentLoaded', init)
+  function init() {
+    // alert("test");
+
+
+    var start = document.querySelector('#start')
+    var reset = document.querySelector('#reset')
+    var pause = document.querySelector('#pause')
+    var seconds = 0;
+    //
+    var timerId = document.querySelector('#timer')
+    var x
+
+    start.addEventListener('click', starter)
+    reset.addEventListener('click', resetter)
+    pause.addEventListener('click', pauser)
+
+    function starter() {
+      // window.setInterval will do updateTime for every 1s.
+      // x = timerId
+      x = window.setInterval(updateTime, 1000)
+      start.addEventListener('click', starter)
+    }
+
+    function resetter() {
+      // windows.clearInterval will make x = 0
+      window.clearInterval(x)
+      seconds = 0
+      timerId.textContent = 'Stop Watch'
+
+    }
+
+    function pauser() {
+      window.clearInterval(x)
+
+    }
+
+    function updateTime(){
+    seconds = seconds + 1
+    timerId.textContent = seconds
+    timerId.id = 'timer'
+  }
+
+  }
